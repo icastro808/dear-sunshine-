99d252cd27a823ffb3cc96bc958a1d00e26fce3a
lucascorriero <lucascorriero@gmail.com>
2024-11-18 20:33:26 -1000

    added background, images and formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+'use client';
 
+import { Container, Image, Row, Button } from 'react-bootstrap';
+import { PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 /** The Home page. */
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className="py-3">
       <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Col>
+        <div className="box">
+          <div className="header">
+            <h1>Dear Sunshine</h1>
+            <Image src="/sunstamp2.png" alt="Sunshine Stamp" className="sun-image" />
+          </div>
+          <p>A platform designed to uplift and encourage individuals who may be going through tough times.</p>
+          <div className="button-group">
+            <Button variant="primary" href="/auth/signin" className="me-2">
+              <PersonFill />
+              Sign In
+            </Button>
+            <Button variant="secondary" href="/auth/signup">
+              <PersonPlusFill />
+              Sign Up
+            </Button>
+          </div>
+        </div>
       </Row>
     </Container>
   </main>
