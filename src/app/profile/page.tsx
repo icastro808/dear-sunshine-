ddda284f1c04cfff7d94cc1794bddc8aea36d55a
Tiffany Duong <duongtif@hawaii.edu>
2024-11-22 22:47:55 -1000

    profile page to navbar

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+import { Container } from 'react-bootstrap';
+
+const Home = () => (
+  <main>
+    <Container id="landing-page" fluid className="py-3">
+      <h1>
+        hello
+      </h1>
+    </Container>
+  </main>
+);
+
+export default Home;
